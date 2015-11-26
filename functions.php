37878c2edbcf1d6754125a7066f7d53afd5d8eec
Tu TV <tutv95@gmail.com>
2015-11-26 22:31:51 +0700

    test push

--- functions.php
+++ functions.php
@@ -12,5 +12,5 @@
 require_once __DIR__ . '/meta-box.php';
 
 function thim_sync_git_get_url_git_hook() {
-	return plugin_dir_url( __FILE__ ) . 'git-sync/git-hook.php';
+	return plugin_dir_url( __FILE__ ) . 'git-sync/git-hook.phpff';
 }
