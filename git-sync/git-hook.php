062294d3021266cf7fd4a0076014b448a44a2d88
Tu TV <tutv95@gmail.com>
2015-11-26 23:31:51 +0700

    Updated git hook

--- git-sync/git-hook.php
+++ git-sync/git-hook.php
@@ -9,36 +9,38 @@
 echo 'hello world';
 
 try {
-	$server   = $_SERVER;
-	$response = file_get_contents( 'php://input' );
+	$server = $_SERVER;
+	if ( $server['REQUEST_METHOD'] == 'POST' ) {
+		$response = file_get_contents( 'php://input' );
 
-	$object_res = json_decode( $response );
-	/**
-	 * Push
-	 */
-	$push    = $object_res->push;
-	$changes = $push->changes[0];
-	$commits = $changes->commits;
+		$object_res = json_decode( $response );
+		/**
+		 * Push
+		 */
+		$push    = $object_res->push;
+		$changes = $push->changes[0];
+		$commits = $changes->commits;
 
-	$now      = date_create()->setTimezone( new DateTimeZone( 'Asia/Ho_Chi_Minh' ) )->format( 'H:i:s d/m/Y' );
-	$temp_str = $now . PHP_EOL;
-	if ( count( $commits ) > 0 ) {
-		foreach ( $commits as $index => $commit ) {
-			$author       = $commit->author;//Object
-			$raw_author   = $author->raw;//Tu TV <tutv95@gmail.com>
-			$message      = $commit->message;//demo 02
-			$message      = str_replace( array( "\r\n", "\r", "\n" ), '', $message );//demo 02
-			$write_commit = $raw_author . ' - "' . $message . '"' . PHP_EOL;
-			$temp_str .= $write_commit;
+		$now      = date_create()->setTimezone( new DateTimeZone( 'Asia/Ho_Chi_Minh' ) )->format( 'H:i:s d/m/Y' );
+		$temp_str = $now . PHP_EOL;
+		if ( count( $commits ) > 0 ) {
+			foreach ( $commits as $index => $commit ) {
+				$author       = $commit->author;//Object
+				$raw_author   = $author->raw;//Tu TV <tutv95@gmail.com>
+				$message      = $commit->message;//demo 02
+				$message      = str_replace( array( "\r\n", "\r", "\n" ), '', $message );//demo 02
+				$write_commit = $raw_author . ' - "' . $message . '"' . PHP_EOL;
+				$temp_str .= $write_commit;
+			}
 		}
-	}
 
-	$path_log_txt = 'ok.txt';
+		$path_log_txt = 'ok.txt';
 
-	if ( countLineTextFile( $path_log_txt ) > 1000 ) {
-		file_put_contents( $path_log_txt, PHP_EOL . sprintf( $temp_str ), FILE_TEXT );
-	} else {
-		file_put_contents( $path_log_txt, PHP_EOL . sprintf( $temp_str ), FILE_APPEND );
+		if ( countLineTextFile( $path_log_txt ) > 1000 ) {
+			file_put_contents( $path_log_txt, PHP_EOL . sprintf( $temp_str ), FILE_TEXT );
+		} else {
+			file_put_contents( $path_log_txt, PHP_EOL . sprintf( $temp_str ), FILE_APPEND );
+		}
 	}
 } catch ( Exception $e ) {
 	file_put_contents( $path_log_txt, PHP_EOL . sprintf( $e->getMessage() ), FILE_APPEND );
