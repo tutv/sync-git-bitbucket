f0776c4505c66e47e40d13600ff71549f9e189d1
Tu TV <tutv95@gmail.com>
2015-11-26 23:09:24 +0700

    Updated log

--- git-sync/git-hook.php
+++ git-sync/git-hook.php
@@ -27,12 +27,14 @@ try {
 			$author     = $commit->author;//Object
 			$raw_author = $author->raw;//Tu TV <tutv95@gmail.com>
 			$message    = $commit->message;//demo 02
-			$message    = str_replace( '\n', '', $message );//demo 02
+			$message    = str_replace( array( "\r\n", "\r", "\n" ), '', $message );//demo 02
 
 			$write_commit = $now . PHP_EOL . $raw_author . ' - "' . $message . '"' . PHP_EOL;
 			$temp_str .= $write_commit;
 		}
 	}
+
+	$temp_str .= PHP_EOL;
 
 
 	file_put_contents( 'ok.txt', $temp_str );
