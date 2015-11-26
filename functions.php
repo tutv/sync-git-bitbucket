0ea483b7335d7c6db91ebf1cc7e34e3f11ddf224
Tu TV <tutv95@gmail.com>
2015-11-26 23:20:19 +0700

    Updated meta box

--- functions.php
+++ functions.php
@@ -11,6 +11,6 @@
  */
 require_once __DIR__ . '/meta-box.php';
 
-function thim_sync_git_get_url_git_hook() {
-	return plugin_dir_url( __FILE__ ) . 'git-sync/git-hook.php';
+function thim_sync_git_get_url_git_hook( $post_id ) {
+	return plugin_dir_url( __FILE__ ) . 'git-sync/git-hook.php?repo=' . $post_id;
 }
