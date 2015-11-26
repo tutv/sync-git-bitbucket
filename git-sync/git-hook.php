743c835939de31a0a94462a825d1ed47a9571f03
Tu TV <tutv95@gmail.com>
2015-11-26 22:58:32 +0700

    Updated log

--- git-sync/git-hook.php
+++ git-sync/git-hook.php
@@ -8,7 +8,32 @@
 
 echo 'hello world';
 
-$server   = $_SERVER;
-$response = file_get_contents( 'php://input' );
+try {
+	$server   = $_SERVER;
+	$response = file_get_contents( 'php://input' );
 
-file_put_contents( 'ok.txt', ( $response ) );
+	$object_res = json_decode( $response );
+	/**
+	 * Push
+	 */
+	$push    = $object_res->push;
+	$changes = $push->changes[0];
+	$commits = $changes->commits;
+
+	$temp_str = '';
+	if ( count( $commits ) > 0 ) {
+		foreach ( $commits as $index => $commit ) {
+			$author     = $commit->author;//Object
+			$raw_author = $author->raw;//Tu TV <tutv95@gmail.com>
+			$message    = $commit->message;//demo 02
+
+			$write_commit = $raw_author . ' - "' . $message . '"' . PHP_EOL;
+			$temp_str .= $write_commit;
+		}
+	}
+
+
+	file_put_contents( 'ok.txt', $temp_str );
+} catch ( Exception $e ) {
+	file_put_contents( 'ok.txt', $e->getMessage() );
+}
