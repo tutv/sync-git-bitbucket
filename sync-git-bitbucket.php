8609b8b71bb0ebfe26aecdb92dce13fe9a7eeaa5
Tu TV <tutv95@gmail.com>
2015-11-26 19:07:19 +0700

    Init

---
+++ sync-git-bitbucket.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Sync Git Bitbucket
+Plugin URI: http://tutran.me
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: tutv95
+Author URI: http://tutran.me
+License: A "Slug" license name e.g. GPL2
+*/
